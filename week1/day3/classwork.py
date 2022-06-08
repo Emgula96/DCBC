75cbf4c55c6ea1c10edf97e51dc26009d5a3aa7c
Emgula96 <ethan_gula@yahoo.com>
2022-06-08 18:48:42 -0500

    day 3

--- week1/day3/classwork.py
+++ week1/day3/classwork.py
@@ -1,8 +1,5 @@
-from argparse import _AppendAction
-from audioop import mul
 
-
-nums = [1,2,-2,-5,-7,3,4,5,6,7,8]
+# nums = [1,2,-2,-5,-7,3,4,5,6,7,8]
 
 # print(sum(nums))
 
@@ -46,5 +43,65 @@ nums = [1,2,-2,-5,-7,3,4,5,6,7,8]
 # reverse = nums[::-1]
 # print (reverse)
 
+# letter = "r"
+# print(ord(letter))
 
 
+
+# nums = [2, 7, 11, 15]
+
+# def twoSum(nums, target):
+#     for i in range(len(nums)):
+#         for j in range(i+1,len(nums)):
+#             result = nums[i]+nums[j]
+#             if result == target:
+#                 return [i,j]
+#     return []
+
+# print(twoSum(nums,26))
+
+
+
+# Square the numbers in this list
+numbers = [1, 2, 3, 4, 5, 6, 7]
+# Output [1, 4, 9, 16, 25, 36, 49]
+
+# multipliedList = [item * 2 for item in numbers]
+# print (multipliedList)
+
+# Remove all empty strings in this list
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+# output ["Mike", "Emma", "Kelly", "Brad"]
+
+# while "" in list1:
+#     list1.remove ("")
+# print(list1)
+
+
+# Add new item to list after a specified item
+list2 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# output [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
+
+
+list2[2][2].append(7000)
+print(list2)
+# Replace list's item with new value if found
+list3 = [5, 10, 15, 20, 25, 50, 20]
+# output [5, 10, 15, 200, 25, 50, 20]
+
+
+# list3[1] = 15
+# print(list3)
+
+# Remove all occurrences of a specific item from a list.
+list4 = [5, 20, 15, 20, 25, 50, 20]
+# output [5, 15, 25, 50]
+
+# while 20 in list4: list4.remove(20)
+# print(list4)
+
+
+# for item in list4:
+#     if item == 20:
+#         list4.remove(20)
+# print(list4)
---
+++ week1/day3/practice.py
@@ -0,0 +1,21 @@
+
+
+
+movies = [('star wars', 1980),('gone with the wind', 1960), ('toy story', 2001), ('buglife', 2004), ('harry potter', 2018), ('glue', 2000), ('raiders', 1986), ('prince of egypt', 1990) ]
+
+# gmovies = [title for title in movies if title.startswith('p')]
+# print(gmovies)
+
+# sub = [(title, year) for (title, year) in movies if year > 2000]
+
+# print(sub)
+
+# v = [2,-3,1]
+# w = [4*x for x in v]
+# print(w)
+
+Apple = [1,3,5,7]
+Bath= [2,4,6,8]
+
+cartProd = [(a,b) for a in Apple for b in Bath]
+print(cartProd)
