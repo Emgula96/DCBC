80e081b6e641af8488e691a8e697ea3da6857c0f
Emgula96 <ethan_gula@yahoo.com>
2022-06-14 15:40:21 -0500

    game fixes

--- week2/day5/Game.py/Game Project/Garfield.py
+++ week2/day5/Game.py/Game Project/Garfield.py
@@ -15,7 +15,8 @@
 
 from time import sleep
 
-
+def stars():
+    print('*****************************************************')
 
 class Character:
     def __init__(self,name,health,attack,items):
@@ -30,7 +31,7 @@ class Character:
 
     def printChar(self):
         # print(self.name,self.health,self.attack,self.items[0])
-        print(f'name:{self.name}, Health:{self.health}, Attack{self.attack},Items{self.items}')
+        print(f'Name: {self.name}, Health: {self.health}, Attack: {self.attack}, Items: {self.items}')
     
     def hitByAttack(self, damage):
         self.health -= damage     
@@ -52,17 +53,26 @@ Jon = Character('Jon', 200, 10, ['cigarette'])
 
 
 def gameStart():
-    
-    print("You wake up in your house like any normal day. ")
-
+    print('\nYou are playing as "Jon" your stats are listed below:\n')
+    Jon.printChar()
+    stars()
+    sleep(7)
+    print('Your once good buddy Garfield has turned into a monster.')
+    sleep(3)
+    print("He is hiding somewhere in the house.....be careful.")
+    sleep(5)
+    print('Oddie has gone missing and you have been locked in your room for protection.')
+    sleep(5)
+    print('You need to put an end to this nightmare, once and for all.') 
+    sleep(5)
     print(
     """
-    Do you want to go to the
-    
+    You slowly unlock your door, where would you like to go?
+
     1. The Kitchen  
-    2. The Study 
-    3. The Garage
-    4. The Neighbors
+    2. The Garage 
+    3. Outside the house
+    4. Quit
 """)
 
     door = input("--> ")
@@ -71,119 +81,96 @@ def gameStart():
     if door == '1':
         kitchenEntry()
     if door == '2':
-        study()
+        garageintro()
     if door == '3':
-        garage()
-    if door == '4':
         outside()
+    if door == '4':
+        print('Garfieled wickedly waves goodbye')
+        
 
 def kitchenEntry():
-    print("You stumble upon Garfield.")
-    print("You notice his lasagna pan is empty...")
-    print('He is hungry....')
-    sleep(2)
-    print('Very.')
-    sleep(2)
-    print('Hungry.')
-    sleep(2)
-    print('You are overwhelmed by a foul stench\n')
-    print('***********************************')
-    sleep(1.5)
-    print ('''What do you do:
-
-    1. Grab the nearby gun
-    2. Fill his lasagna pan
-    3. Inspect lasagna pan''')
+    stars()
+    print('You cautiously make your way into the Kitchen.')
+    sleep(3)
+    print('You notice an empty lasagna pan in the corner.')
+    sleep(3)
+    print('You are overwhelmed by a foul stench.\n')
+    sleep(3)
+    stars()
     kitchen()
 
 def kitchen():
+    sleep(1.5)
+    print('''What do you want do:
 
+        1. Grab the nearby gun
+        2. Search the kitchen drawers
+        3. Inspect the lasagna pan\n''')
+    stars()
     garfield = input('--> ')
     while garfield != '1' and garfield != '2' and garfield != '3':
         garfield = input("Please enter a number 1 - 3 -->  ")
     if garfield == "1":
+        print('You grab the gun and begin to inspect it')
+        sleep(4)
+        print('You look up and see that Garfield has appeared in front of you!')
+        sleep(4)
+        stars()
         print('''Do you?
 
         1. Shoot Garfield
-        2. Fire a warning shot in the air''')
+        2. Fire a warning shot near garfield''')
 
         gun = input('--> ')
         while gun != '1' and gun != '2':
             gun = input("Please enter a number 1 or 2 -->  ")
         if gun == "1":
-            print("You pick up the gun and shoot Garfield")
+            print("You point the gun and shoot Garfield")
             sleep(2)
-            print('''
-
+            print('''\n\n
 The bullet demolishes one of Garfield's now 20 eyes. He suddenly appears as a 
-25 foot monster with spider legs and a dark shroud fills the room.
-
-*****************************************************
-
-''')
+25 foot monster with spider legs and a dark shroud fills the room.''')
+            Jon.attackBigBoi(megaCat)
+            garfieldhit()
             sleep(7)
             print('''
 Garfield is largely unaffected by the 
 bullet and responds in perfect english:''')
             sleep(4)
-            print('''
+            print(' Bullets. Wont. Work.\n')
 
-Bullets...wont...work...Jon.
-
-*****************************************************
-            ''')
+            stars()
             sleep(3)  
-            print('''
-Garfield unhinges his jaws and everything fades to black...
-''')
+            print('Garfield now lunges forward and attacks')
+            Jon.hitByAttack(megaCat.attack)
+            print(f'Your health is now {Jon.health}')
             sleep(3)
         elif gun == '2':
             print('You fire the warning shot.')
             print('Garfield stares at the passing bullet, buying you time to escape.')
             print('You run into the garage')
-            garage()
+            garageintro()
             #some sort of break/escape
         else:
-            print('Please only type a number 1 - 3')
+            print('Please only type a number 1 - 2')
         
 
     elif garfield == '2':
-        print('You fill Garfields pan')
-        print('''
-With malice in his voice, Garfield responds: 
-"This does not satiate me, human I demand more"''')
-        print('''What do you do?
-
-        1. Beg for mercy
-        2. Go to the store and buy more lasagna''')
-
-        gar2 = input('-->' )
-        if gar2 == '1':
-            print('Garfield responds "I am your mercy"')
-            print('Everything fades to black...')
-
-        elif gar2 == "2":
-            print('You manage to escape the house, and you set it on fire.')
-            print('''
-Ermerging from the flames are 40ft long bat wings, 
-Garfield flys towards the heavens never to be seen again.
-            ''')
-            sleep(10)
-            print('For now......')
-            #printbreak/end
+        print('You search the drawers')
 
     elif garfield == '3':
-        print('You walk over and fill the lasagna pan.')
+        print('You walk over and peer into the lasagna pan. The smell grows more intense.')
         print('You notice some strange remains at the bottom of the pan.')
         sleep(3)
         print('''What do you want to do?
 
-        1. Leave the room under the pretenses of getting more lasagna
-        2. Continue inspecting pan.''')
+        1. Go back
+        2. Continue inspecting pan
+        ''')
         
         gar3 = input('-->' )
         if gar3 == '1':
-            print('You leave the room and live the rest of your life in fear.')
+            kitchen()
         if gar3 == '2':
             print("You feel a malevolant force push you into the pan.")
             sleep(3)
@@ -191,7 +178,7 @@ Garfield flys towards the heavens never to be seen again.
             sleep(3)
             print('He has a wide grin and says "You are the lasagna now, Jon."')
             sleep(3)
-            print("The world fades to black....")
+            death()
 
 def preface():
     print(''' 
@@ -211,28 +198,24 @@ created by Ethan Gula with a very basic understanding of Python 3, copyright 202
             break
 
 def mainMenu():
-    while True:
-        print('\n   Welcome to "GARFIELD: LARGER THAN LIFE" ')
-        sleep(3)
-        print("""
-                Main Menu\n\n
-        1. Play
-        2. Preface
-        3. Options
-        4. Exit
-        """)
-        choice = input("Indicate how you wish to proceed by typing in the number you've chosen \n --> ")
-        if choice == '1':
-            gameStart()
-        if choice == '2':
-            preface()
-        if choice == '3':
-            options()
-        if choice == '4':
-            print('Goodbye' )
-            break
-        else:
-            print('Please choose a number 1- 4\n')
+    print('\n   Welcome to "GARFIELD: LARGER THAN LIFE" ')
+    sleep(3)
+    print("""
+            Main Menu\n\n
+    1. Play
+    2. Preface
+    3. Exit
+    """)
+    choice = input("Indicate how you wish to proceed by typing in the number you've chosen \n --> ")
+    if choice == '1':
+        gameStart()
+    elif choice == '2':
+        preface()
+    elif choice == '3':
+        print('Goodbye' )
+        
+    else:
+        print('Please choose a number 1- 3\n')
 
 def death():
     while True:
@@ -248,12 +231,14 @@ def death():
         else: 
             print('Please enter "Y"or "N" \n -->')
 
-def garage():
-    print('You stubmle into the garage and notice the lights are flickering... ')
+def garageintro():
+    print('You stumble into the garage and notice the lights are flickering... ')
+    garageOptions()
+def garageOptions():
     print('''What do you do?
-    1.Walk over to the electrical box
-    2.Walk over to the toolbox
-    3.Inspect the weird bag in the corner
+        1. Walk over to the electrical box
+        2. Fill a gasoline canister
+        3. Inspect the weird bag in the corner
     ''')
     garChoice = input('--> ')
     while garChoice != '1' and garChoice != '2' and garChoice != '3':
@@ -268,10 +253,14 @@ def garage():
         while elect != '1' and elect != '2':
             elect = input("Please enter a number 1 or 2 -->  ")
         if elect == '1':
-            garage()
+            print ('You are now back in the center of the garage')
+            garageOptions()
         elif elect == '2':
             print('''
-While trying to force the box open you accidentally touch some live wires and the world fades to black...''')
+While trying to force the box open you accidentally touch some live wires.''')
+        Jon.hitByAttack(50)
+        print(f'Your new health is {Jon.health}')
+        garageOptions()
     elif garChoice == '2':
         print('')
 
