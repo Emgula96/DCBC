d8667dd3bd3c3136aa9bc2be8afcd5e40f9b0908
Emgula96 <ethan_gula@yahoo.com>
2022-08-16 11:30:51 -0400

    routes in react

--- week11/day1/first-react-project/src/Body.js
+++ week11/day1/first-react-project/src/Body.js
@@ -3,19 +3,7 @@ import { useState } from 'react'
 
 
 export default function Body() {
-    const [input, setInput] = useState('');
-    const [todo, setTodo] = useState([]);
-    const [todoItem,setToDoItem] = useState("")
 
-    const addToDo = () => {
-        setTodo(...todo, todoItem);
-        setToDoItem("");
-    };
-
-    const deleteToDo = (item) => {
-        const filteredTodoList = todo.filter((todo) => todo !== item)
-        // setToDo(filteredTodoList);
-    }
 
     return (
         <div className='body'>
@@ -28,15 +16,6 @@ export default function Body() {
                 className='todo-inpout' />
             <button className='add-todo'>Add To List</button>
         </form> */}
-            <h1>Add a to do item</h1>
-            <input value={todoItem} onChange={(e) => setToDoItem(e.target.value)} type="text" />
-            <button onClick={() => addToDo()}>Submit</button>
-            {todo.map((todo, i) => (
-                <>
-                <p>{todo}</p>
-                    <button onClick={() => deleteToDo(todo)}>X</button>
-                </>
-            ))}
         </div>
     );
 }
---
+++ week11/day2/react-router/src/components/ProductCard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ProductCard = (props) => {
+    return <div>ProductCard {props?.id}</div>
+};
+
+export default ProductCard;
